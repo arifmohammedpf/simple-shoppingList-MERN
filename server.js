a1716ff530fc86b73bd09b2902752c71be1b8559
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-10-16 10:39:50 +0530

    Backend - basics done Frontend - Navbar

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const env = require('dotenv')
+
+const itemsRoute = require('./routes/api/items')
+
+env.config()
+
+//app config
+const app = express()
+
+//middleware
+app.use(express.json())
+
+//DB config
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(()=>console.log('DB Connected'))
+    .catch(err=>console.log(err))
+
+//routes
+app.use('/api/items',itemsRoute)
+
+
+//listener
+const port = process.env.PORT || 9000
+app.listen(port,()=>console.log(`Server Started in ${port}`))
