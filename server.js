3e0f661ffc5412c83e678b6ed510386c01838b5b
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-10-17 21:48:56 +0530

    Changes made for Heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const env = require('dotenv')
+const path = require('path')
 
 const itemsRoute = require('./routes/api/items')
 
@@ -21,6 +22,15 @@ mongoose
 //routes
 app.use('/api/items', itemsRoute)
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 //listener
 const port = process.env.PORT || 9000
